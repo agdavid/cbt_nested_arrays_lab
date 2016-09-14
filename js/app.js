a4f918e432d17353debf9d0ee6bca19edca9226b
agdavid <antonio.g.david@gmail.com>
2016-09-14 08:52:02 -0400

    Done. Iterate over nested arrays and display data.

--- js/app.js
+++ js/app.js
@@ -2,15 +2,35 @@ var prog1 = [];
 prog1["Name"] = "Matz";
 
 var prog2 = [];
-prog1["Name"] = "Ada";
+prog2["Name"] = "Ada";
 
 var prog3 = [];
-prog1["Name"] = "Mark";
+prog3["Name"] = "Mark";
 
 var progs = [prog1, prog2, prog3];
 
 function displayProgs() {
-  console.log("In displayProgs")
+  
+  var n = progs.length;
+  
+  var ResultsDiv = document.getElementById('resultsDiv');
+
+  //iterate over employees to get each employee
+  for(var i = 0; i < n; i++) {
+
+    var emp = progs[i];
+
+    //iterate over employee to get each key/value
+    for(var key in emp) {
+      
+      var ChildDiv = document.createElement('div');
+      ChildDiv.innerHTML = key + ": " + emp[key];
+      ResultsDiv.appendChild(ChildDiv);
+    }
+    //add break for better display
+    var ChildBr = document.createElement('br');
+    ResultsDiv.appendChild(ChildBr);
+  }
 };
 
 
