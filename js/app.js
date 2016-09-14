2ac68f0eb64a72f230c247295d3801edeb32c58a
agdavid <antonio.g.david@gmail.com>
2016-09-14 08:39:33 -0400

    add initial array data to JS, hooks to HTML

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var prog1 = [];
+prog1["Name"] = "Matz";
+
+var prog2 = [];
+prog1["Name"] = "Ada";
+
+var prog3 = [];
+prog1["Name"] = "Mark";
+
+var progs = [prog1, prog2, prog3];
+
+function displayProgs() {
+  console.log("In displayProgs")
+};
+
+
